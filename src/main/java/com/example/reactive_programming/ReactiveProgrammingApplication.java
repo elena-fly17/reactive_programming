c87f5e19c57c861575e2385d4fdcac43ec533695
elena-fly17 <elena17mr@gmail.com>
2023-06-30 18:05:03 +0300

    Второй вариант выполнения 2 задачи (использование устаревшей вариации метода subscribe, принимающего 4 параметра)

--- src/main/java/com/example/reactive_programming/ReactiveProgrammingApplication.java
+++ src/main/java/com/example/reactive_programming/ReactiveProgrammingApplication.java
@@ -17,6 +17,13 @@ public class ReactiveProgrammingApplication {
 //		Task_1 task_1 = new Task_1();
 //		task_1.incrementIfMoreThenFive(flux);
 
+		// проверка работы метода processFiveElements с использованием
+		// современного метода subscribeWith
+//		Task_2 task_2 = new Task_2();
+//		task_2.processFiveElements(flux);
+
+		// проверка работы метода processFiveElements с использованием
+		// устаревшей вариации метода subscribe, принимающего 4 параметра
 		Task_2 task_2 = new Task_2();
 		task_2.processFiveElements(flux);
 	}
--- src/main/java/com/example/reactive_programming/tasks/Task_2.java
+++ src/main/java/com/example/reactive_programming/tasks/Task_2.java
@@ -1,15 +1,36 @@
 package com.example.reactive_programming.tasks;
 
-import com.example.reactive_programming.subscribers.Subscriber_Task_2;
 import lombok.Data;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
 
 @Data
 public class Task_2 {
 
+    Subscription mySubscription;
+
+    // вариант с использованием современного метода subscribeWith
+//    public void processFiveElements(Flux<Integer> flux) {
+//
+//        Subscriber_Task_2<Integer> mySubscriber = new Subscriber_Task_2<>();
+//        flux.subscribeWith(mySubscriber);
+//    }
+
+    // вариант с использованием устаревшей вариации метода subscribe (его варианта,
+    // принимающего 4 параметра)
     public void processFiveElements(Flux<Integer> flux) {
 
-        Subscriber_Task_2<Integer> mySubscriber = new Subscriber_Task_2<>();
-        flux.subscribeWith(mySubscriber);
+        flux.subscribe(
+                value -> {
+                    System.out.println(value);
+                    mySubscription.request(5);
+                },
+                error -> System.out.println(error.getMessage()),
+                () -> System.out.println("Done"),
+                subscription -> {
+                    this.mySubscription = subscription;
+                    subscription.request(5);
+                }
+        );
     }
 }
